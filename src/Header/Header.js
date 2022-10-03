89226c3f381654656bddf2d55169edb818437940
Egor Konovalov <agegorin@gmail.com>
2022-10-03 15:31:58 +0400

    update fonts styles

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ import BackButton from "../BackButton/BackButton";
 const Header = (props) => {
   return <div className="Header">
     <BackButton />
-    <h1 className="HeaderTitle">{props.children}</h1>
+    <h1 className="Header_title">{props.children}</h1>
   </div>
 }
 
